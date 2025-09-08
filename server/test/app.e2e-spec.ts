af1a137d1c9584300acda03112f4ed3be3241f8e
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-09-08 14:41:28 +0200

    correct basic e2e test with better ts imports

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
+import { Response } from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
@@ -16,10 +17,9 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', async () => {
+    const res:Response = await request(app.getHttpServer()).get('/');
+  expect(res.status).toBe(200);
+  expect(res.text).toBe('Hello World!');
   });
 });
